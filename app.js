f26bb0db2882eaa9e00dfa3155a3b66a0530f357
Have Samuel <samiehave9@gmail.com>
2023-06-27 01:46:54 +0300

    Getting the Right ApI URL

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-function getData() {
-  const response = axios.get('https://swapi.co/api/planets/');
-  for (let planet of response.data.results) {
-      console.log(planet.name)
+async function getData() {
+  const response = await axios.get('https://swapi.dev/api/planets/');
+  for (const planet of response.data.results) {
+    console.log(planet);
   }
 }
 
 // This shows up faster than the async/await funnction
 getData();
-console.log('I HAPPEN AFTER getData()')
+console.log('I HAPPEN AFTER getData()');
