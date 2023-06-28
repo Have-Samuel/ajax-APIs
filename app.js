69df5b8f2727879febe15e643d36dee489a35914
Have Samuel <samiehave9@gmail.com>
2023-06-28 23:37:06 +0300

    New Dog fetch function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 async function getRandomDog() {
   const resp = await axios.get('https://dog.ceo/api/breeds/image/random');
   console.log(resp.data);
+  const img = document.querySelector('#dog');
+  img.src = resp.data.message;
 }
 
 getRandomDog();
