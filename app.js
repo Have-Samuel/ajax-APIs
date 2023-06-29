2707f521eb0cbe5907927754a4f4f62dd865a126
Have Samuel <samiehave9@gmail.com>
2023-06-29 17:23:21 +0300

    With GET requests, you can add data for the request in the query string

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
 /* eslint-disable no-undef */
-async function getRandomDog() {
-  const resp = await axios.get('https://dog.ceo/api/breeds/image/random');
-  console.log(resp.data);
-  const img = document.querySelector('#dog');
-  img.src = resp.data.message;
+async function getJokes() {
+  const res = await axios.get('https://api.chucknorris.io/jokes/random');
+  console.log(res.data.value);
 }
 
-async function getDogByBreed(breed) {
-  try {
-    const url = `https://dog.ceo/api/breed/${breed}/images/random`;
-    const resp = await axios.get(url);
-    const img = document.querySelector('#dog');
-    img.src = resp.data.message;
-  } catch (err) {
-    // console.log(err);
-    alert('Breed not found!');
-    getRandomDog();
-  }
-}
-// getRandomDog();
-// getDogByBreed('bulldog');
-
-const form = document.querySelector('#searchForm');
-const input = document.querySelector('#search');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  console.log('SUBMIT!');
-  getDogByBreed(input.value);
-  input.value = '';
-});
+getJokes();
