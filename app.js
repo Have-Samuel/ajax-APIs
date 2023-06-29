4a11e45f8054358a9173452c882e167cef3f23b9
Have Samuel <samiehave9@gmail.com>
2023-06-29 19:05:51 +0300

    Deleted the file

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 /* eslint-disable no-undef */
-async function getJokes() {
-  const res = await axios.get('https://api.chucknorris.io/jokes/random');
-  console.log(res.data.value);
+// async function getUsers() {
+//   const res = await axios.get('https://hack-or-snooze.herokuapp.com/users');
+//   console.log(res);
+// }
+
+async function signUp(userName, password, name) {
+  const res = await axios.post('https://hack-or-snooze-v3.herokuapp.com/signup', { user: { userName, password, name } });
+  console.log(res);
 }
 
-getJokes();
+async function login(userName, password) {
+  const res = await axios.post('https://hack-or-snooze-v3.herokuapp.com/signup', { user: { userName, password } });
+  console.log(res);
+}
+
+signUp('test-1', 'test-2', 'test-3');
+login('test-1', 'test-2');
+// getUsers();
